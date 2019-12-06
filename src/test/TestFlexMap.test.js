ad13af400fb2d9e1f503887dd10bef975d8300cc
Basheitan <antoine@flexio.fr>
2019-12-06 17:11:48 +0100

    add mmap with/without (for deepFreezeSeal)

--- src/test/TestFlexMap.test.js
+++ src/test/TestFlexMap.test.js
@@ -1,6 +1,7 @@
 /* global runTest */
 import {TestCase} from 'code-altimeter-js'
 import {FlexMap} from '../js/FlexMap'
+import {deepFreezeSeal} from '@flexio-oss/js-generator-helpers'
 
 const assert = require('assert')
 
@@ -20,6 +21,26 @@ export class TestFlexMap extends TestCase {
     let map = new TestMap()
     map.set(8, 9)
     assert.strictEqual(map.get(8), 9)
+  }
+
+  testDeepFreezeAdd() {
+    let map = new TestMap()
+    map.set(8, 9)
+    deepFreezeSeal(map)
+    map.with(9, 10)
+    assert.strictEqual(map.get(9), null)
+    map = map.with(9, 10)
+    assert.strictEqual(map.get(9), 10)
+  }
+
+  testDeepFreezeDelete() {
+    let map = new TestMap()
+    map.set(8, 9)
+    deepFreezeSeal(map)
+    map.without(8)
+    assert.strictEqual(map.get(8), 9)
+    map = map.without(8)
+    assert.strictEqual(map.get(8), null)
   }
 }
 
