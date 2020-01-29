a0f5a5dc7a324f1ff18db8cd839ec2000b236ef9
tomacht <thomas@flexio.fr>
2020-01-29 12:09:26 +0100

    add tests

--- src/test/TestFlexDate.test.js
+++ src/test/TestFlexDate.test.js
@@ -13,6 +13,20 @@ export class TestFlexDate extends TestCase {
     assert.strictEqual(date.toJSON(), '1992-10-17', 'test date creation')
   }
 
+  testDateSerialize() {
+    let date = new FlexDate('1992-10-17')
+    assert.strictEqual(JSON.parse(JSON.stringify(date)), '1992-10-17', 'FlexDate serialize')
+
+    let time = new FlexTime('04:17:32.527')
+    assert.strictEqual(JSON.parse(JSON.stringify(time)), '04:17:32.527', 'FlexTime serialize')
+
+    let flexDateTime = new FlexDateTime('1992-12-17T04:17:32')
+    assert.strictEqual(JSON.parse(JSON.stringify(flexDateTime)), '1992-12-17T04:17:32', 'FlexDateTime serialize')
+
+    let flexZonedDateTime = new FlexZonedDateTime('1992-10-17T04:17:32+03:00')
+    assert.strictEqual(JSON.parse(JSON.stringify(flexZonedDateTime)), '1992-10-17T04:17:32+03:00', 'FlexZonedDateTime serialize')
+  }
+
   testTimeCreation() {
     let time = new FlexTime('04:17:32.527')
     assert.strictEqual(time.toJSON(), '04:17:32.527', 'test time creation')
